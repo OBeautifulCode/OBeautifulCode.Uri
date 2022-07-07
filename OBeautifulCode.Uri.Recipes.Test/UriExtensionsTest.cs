e6a0ab5bfdda604ad6a410bdbe17b4324b862ba9
Suraj Gupta <SurajGupta@users.noreply.github.com>
2022-07-07 12:10:17 -0400

    added GetNakedDomain

--- OBeautifulCode.Uri.Recipes.Test/UriExtensionsTest.cs
+++ OBeautifulCode.Uri.Recipes.Test/UriExtensionsTest.cs
@@ -7,21 +7,74 @@
 namespace OBeautifulCode.Uri.Recipes.Test
 {
     using System;
-
+    using System.Linq;
     using OBeautifulCode.Assertion.Recipes;
-
     using Xunit;
 
     public static class UriExtensionsTest
     {
         [Fact]
-        public static void Method___Should_do_something___When_called()
+        public static void GetNakedDomain___Should_throw_ArgumentNullException___When_parameter_uri_is_null()
         {
-            // Arrange
-
-            // Act
+            // Arrange, Act
+            var actual = Record.Exception(() => UriExtensions.GetNakedDomain(null));
 
             // Assert
+            actual.AsTest().Must().BeOfType<ArgumentNullException>();
+        }
+
+        [Fact]
+        public static void GetNakedDomain___Should_throw_ArgumentException___When_parameter_uri_is_a_relative_URI()
+        {
+            // Arrange
+            var uri = new Uri("example.com", UriKind.Relative);
+
+            // Act
+            var actual = Record.Exception(() => uri.GetNakedDomain());
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentException>();
+            actual.Message.AsTest().Must().BeEqualTo("The specified URI is a relative URI; it must be absolute: example.com.");
+        }
+
+        [Fact]
+        public static void GetNakedDomain___Should_throw_ArgumentException___When_parameter_uri_HostNameType_is_not_Dns()
+        {
+            // Arrange
+            var uris = new[]
+            {
+                new Uri("http://[::1]:8080", UriKind.Absolute), // Ipv6
+                new Uri("http://192.168.0.1:8080", UriKind.Absolute), // Ipv4
+                new Uri("c:\\my\\file", UriKind.Absolute), // Basic
+            };
+
+            // Act
+            var actual = uris.Select(_ => Record.Exception(_.GetNakedDomain));
+
+            // Assert
+            actual.AsTest().Must().Each().BeOfType<ArgumentException>();
+            actual.Select(_ => _.Message).AsTest().Must().Each().ContainString("The specified URI host name type is not Dns; it is");
+        }
+
+        [Fact]
+        public static void GetNakedDomain___Should_return_naked_domain___When_called()
+        {
+            // Arrange
+            var expected = "example.com";
+
+            var uris = new[]
+            {
+                new Uri("https://example.com", UriKind.Absolute),
+                new Uri("https://www.example.com", UriKind.Absolute),
+                new Uri("https://test.www.example.com", UriKind.Absolute),
+                new Uri("https://user:password@www.example.com:80/Home/Index.htm?q1=v1&q2=v2#FragmentName"),
+            };
+
+            // Act
+            var actual = uris.Select(_ => _.GetNakedDomain()).ToList();
+
+            // Assert
+            actual.AsTest().Must().BeEqualTo(Enumerable.Repeat(expected, uris.Length).ToList());
         }
     }
 }
--- OBeautifulCode.Uri.Recipes/UriExtensions.cs
+++ OBeautifulCode.Uri.Recipes/UriExtensions.cs
@@ -9,6 +9,9 @@
 
 namespace OBeautifulCode.Uri.Recipes
 {
+    using global::System;
+    using static global::System.FormattableString;
+
     /// <summary>
     /// Extension methods on <see cref="System.Uri"/>.
     /// </summary>
@@ -19,7 +22,45 @@ namespace OBeautifulCode.Uri.Recipes
 #else
     public
 #endif
-    class UriExtensions
+    static class UriExtensions
     {
+        /// <summary>
+        /// Gets the naked domain from a URI.
+        /// </summary>
+        /// <remarks>
+        /// Adapted from <a href="https://weblog.west-wind.com/posts/2012/Apr/24/Getting-a-base-Domain-from-a-Domain"/>.
+        /// </remarks>
+        /// <param name="uri">The URI.</param>
+        /// <returns>
+        /// The naked domain.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="uri"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="uri"/> is a relative URI.</exception>
+        public static string GetNakedDomain(
+            this System.Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            if (!uri.IsAbsoluteUri)
+            {
+                throw new ArgumentException(Invariant($"The specified URI is a relative URI; it must be absolute: {uri.OriginalString}."));
+            }
+
+            if (uri.HostNameType != UriHostNameType.Dns)
+            {
+                throw new ArgumentException(Invariant($"The specified URI host name type is not {nameof(System.UriHostNameType.Dns)}; it is {uri.HostNameType}: {uri.OriginalString}."));
+            }
+
+            var tokens = uri.DnsSafeHost.Split('.');
+
+            var result = tokens.Length > 2
+                ? tokens[tokens.Length - 2] + "." + tokens[tokens.Length - 1]
+                : uri.DnsSafeHost;
+
+            return result;
+        }
     }
 }
